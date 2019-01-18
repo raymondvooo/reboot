8c67c9af04a13af11ce8b71c302e1631c9bd6445
Robert Brown <robertlb4@gmail.com>
2019-01-17 17:27:24 -0800

    implement logic for offline interceptor

--- src/app/http-interceptors/offlineInterceptor.ts
+++ src/app/http-interceptors/offlineInterceptor.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpEventType, HttpResponse } from '@angular/common/http';
-
 import { Observable } from 'rxjs';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/concatMap'
 import { fromPromise } from 'rxjs/observable/fromPromise';
-import 'rxjs/add/observable/of'
 import { NetworkProvider, ConnectionStatusEnum } from '../../providers/network/network';
 import { ENV } from '@app/env';
 import { StorageProvider } from '../../providers/storage/storage';
@@ -12,17 +12,53 @@ import { StorageProvider } from '../../providers/storage/storage';
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class OfflineInterceptor implements HttpInterceptor {
-
+  storage: Observable<any>
   constructor(private _network: NetworkProvider, private _storage: StorageProvider ){}
 
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
-      if (req.url === 'https://api.rss2json.com/v1/api.json') return next.handle(req);
-      if(this._network.previousStatus == ConnectionStatusEnum.Offline || ENV.mode === 'Development') {
-        this._storage.storeRequest(req)
-        return Observable.throw(new HttpErrorResponse({error: 'Operating on offline mode'}))
+      if (!(req.headers.get('cacheKey'))) {
+        return next.handle(req)
       }
-      return next.handle(req)
+      if (this._network.previousStatus === ConnectionStatusEnum.Offline) {
+
+        this.storage = this._storage.storeRequest(req)
+        this.storage.subscribe(res => console.log(res))
+        let data;
+        switch (req.method) {
+          case 'GET':
+          return this._storage.retrieveFromStorageObs(req.headers.get('cacheKey'))
+            .mergeMap(storedItem => {
+              data = storedItem 
+              return data? this.createNewRequest(req, data): next.handle(req)
+            })
+          case "POST":
+            if(req.url.includes('appUsers/logout')) {
+              this._storage.emptyStorage()
+            }
+          default:
+          return this.createNewRequest(req, data)
+        }
+      } else {
+        req = req.clone({headers: req.headers.delete('cacheKey')})
+        return next.handle(req);
+      }
+  }
+
+
+  createNewRequest(req, data) {
+    let event = new HttpResponse({
+      body: data,
+      status: 200,
+      statusText: 'request cached. retrieved local data',
+      url: req.url,
+    })
+
+    let Obs = Observable.create((observer:any) => {
+      observer.next(event)
+  })
+
+    return Obs
   }
 
 }
