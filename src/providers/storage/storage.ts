f18a052e650aa84406427f8808ad7f814a2a00ad
Robert Brown <robertlb4@gmail.com>
2019-01-17 17:32:14 -0800

    updates to begin fulfilling cached requests

--- src/providers/storage/storage.ts
+++ src/providers/storage/storage.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
 import { fromPromise } from 'rxjs/observable/fromPromise';
+import 'rxjs/add/operator/mergeMap';
 import { ToastController } from 'ionic-angular';
-import * as moment from 'moment';
-import { Observable } from 'rxjs';
+// import * as moment from 'moment';
+
 
 
 const STORAGE_REQ_KEY = 'cachedReq';
@@ -11,10 +13,11 @@ const STORAGE_REQ_KEY = 'cachedReq';
 @Injectable()
 export class StorageProvider {
 
-  constructor(private storage: Storage, private toastCtrl: ToastController) {}
+  constructor(private storage: Storage, private toastCtrl: ToastController, private _http: HttpClient) {}
 
 
   storeRequest(req) {
+    // console.log('request stored')
     let toast = this.toastCtrl.create({
       message: `Your data is stored locally because you seem to be offline.`,
       duration: 3000,
@@ -22,48 +25,77 @@ export class StorageProvider {
     });
     toast.present()
  
-    return this.storage.get(STORAGE_REQ_KEY).then(storedOperations => {
-      let action = {
-        url: req.url,
-        data: req.body,
-        type: req.method,
-        date:  moment().format('MMMM Do YYYY, h:mm:ss a'),
-        id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
-      }
+    return fromPromise(this.storage.get(STORAGE_REQ_KEY))
+      .mergeMap(storedOperations => {
+        let action = {
+          url: req.url,
+          method: req.method,
+          body: req.body || {},
+          params: req.params || {},
+          cacheKey: req.headers.get('cacheKey')
+          // date:  moment().format('MMMM Do YYYY, h:mm:ss a'),
+          // id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
+        }
+        let storedObj
+        if (storedOperations) {
+          storedObj = storedOperations.concat([action])
+        } else {
+          storedObj = [action];
+        }
+        return fromPromise(this.storage.set(STORAGE_REQ_KEY, storedObj))
+      });
+  }
 
-      let storedObj
-      if (storedOperations) {
-        storedObj = storedOperations.concat([action])
-      } else {
-        storedObj = [action];
-      }
-      return this.storage.set(STORAGE_REQ_KEY, storedObj)
-    });
+  completeCachedRequests() {
+    this.retrieveFromStorage(STORAGE_REQ_KEY)
+      .then(storedReqs => {
+        if(storedReqs && storedReqs.length > 0) {
+          storedReqs.array.forEach((req, i) => {
+            this._http.request(req.method, req.url, {body: req.body, params: req.params})
+              .subscribe(res => {
+                if (req.method === 'GET') {
+                  this.addToStorageItemObs(req.cacheKey, res)
+                }
+              })
+          });
+        }
+      })
   }
 
 
-  saveToLocalStorage(key, value) {
+  saveToStorage(key, value) {
     return this.storage.set(key, value)
   }
 
-  retrieveFromLocalStorage(key) {
-    return this.storage.get(key).then((val) => {
-    })
+  saveToStorageObs(key, value) {
+    return fromPromise(this.storage.set(key, value));
   }
 
-  deleteFromLocalStorage (key) {
+  retrieveFromStorage(key) {
+    return this.storage.get(key)
+  }
+
+  retrieveFromStorageObs(key) {
+    return (fromPromise(this.storage.get(key)))
+  }
+
+  deleteFromLocalStorage(key) {
     return this.storage.remove(key)
   }
 
-  addToItem(key, value): any {
+  addToStorageItemObs(key, value): any {
     return fromPromise(this.storage.get(key)
-      .then((val) => {
+      .then((val) => {  
         if(val) {
           value = val.concat([value])
           return this.storage.set(key, value)
-        }
+        } 
           return this.storage.set(key, [value])
       }))
+  }
+
+  emptyStorage() {
+    this.storage.clear()
   }
 
 }
