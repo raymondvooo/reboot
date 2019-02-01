bb39689637c85b786bd0afd22d13a040e242499b
Gino17 <galentro451@gmail.com>
2019-01-31 17:48:07 -0800

    able to pass value from timeline component to timeline child sublist to display sublist items based on indexvalue

--- src/components/timeline-child-sublist/timeline-child-sublist.ts
+++ src/components/timeline-child-sublist/timeline-child-sublist.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Segment } from 'ionic-angular';
 
 @Component({
   selector: 'timeline-child-sublist',
@@ -6,27 +7,71 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class TimelineChildSublistComponent {
 
-  @Input() testInput: number;
+  somenumber: number = 0
+
+  @Input() segmentValue: number;
 
   constructor() {
+    console.log(this.segmentValue)
   }
 
   public childList: any[] = [
     {
-      title: 'test0'
+      indexValue: 0,
+      titleSubject: 'Understanding the process of separating'
     },
     {
-      title: 'test1'
+      indexValue: 0,
+      titleSubject: 'Completing my paperwork'
     },
     {
-      title: 'test2'
+      indexValue: 0,
+      titleSubject: 'Engaging VA to access benefits'
     },
     {
-      title: 'test3'
+      indexValue: 1,
+      titleSubject: 'Find something to do'
     },
     {
-      title: 'test4'
-    }
+      indexValue: 1,
+      titleSubject: 'Re-establishing and creating relationships'
+    },
+    {
+      indexValue: 1,
+      titleSubject: 'Balancing finances'
+    },
+    {
+      indexValue: 2,
+      titleSubject: 'Recognizing and addressing mental health needs'
+    },
+    {
+      indexValue: 2,
+      titleSubject: 'VA Health Care'
+    },
+    {
+      indexValue: 2,
+      titleSubject: 'Seeking support for acute health event'
+    },
+    {
+      indexValue: 3,
+      titleSubject: 'Acquiring the appropriate education, new skills, and credentials'
+    },
+    {
+      indexValue: 3,
+      titleSubject: 'Finding the right job'
+    },
+    {
+      indexValue: 4,
+      titleSubject: 'Maintaining my financial, social, and emotional health'
+    },
+    {
+      indexValue: 4,
+      titleSubject: 'Starting, growing, or taking care of my family'
+    },
+    {
+      indexValue: 4,
+      titleSubject: 'Connecting with, and serving my community'
+    },
   ]
 
 }
