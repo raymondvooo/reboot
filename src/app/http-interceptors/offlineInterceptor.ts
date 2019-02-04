d7bcc786a30aec1687ec61339ceb849c2b82cd8c
Robert Brown <robertlb4@gmail.com>
2019-02-04 14:36:05 -0800

    remove unneeded imports

--- src/app/http-interceptors/offlineInterceptor.ts
+++ src/app/http-interceptors/offlineInterceptor.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpEventType, HttpResponse } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/concatMapTo'
-import { fromPromise } from 'rxjs/observable/fromPromise';
 import { NetworkProvider, ConnectionStatusEnum } from '../../providers/network/network';
 import { ENV } from '@app/env';
 import { StorageProvider } from '../../providers/storage/storage';
